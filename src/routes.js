709684901fb769846d5375d904bddc99dfb729cd
mrnobody2002 <filipemelo032@gmail.com>
2020-03-19 12:07:19 -0300

    API Routes setup

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,16 @@
-const {Router} = require('express')
+const { Router } = require("express");
 
+const routes = Router();
 
-const routes = Router()
+const uploadConfig = require("./config/upload");
+const multer = require("multer");
+const upload = multer(uploadConfig);
 
+const SessionController = require("./controllers/SessionController");
+const PictureController = require("./controllers/PictureController");
 
-const SessionController = require('./controllers/SessionController')
-
-routes.post('/admin', SessionController.index)
-
-
-
-module.exports = routes
+routes.post("/admin", SessionController.index);
+routes.post("/pictures", upload.single("picture"), PictureController.store);
+routes.get("/pictures", PictureController.index);
+routes.delete("/pictures/:id/:picture", PictureController.delete);
+module.exports = routes;
