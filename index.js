ea4fb7b07e02fdc9c76882397b616fe92b58b5d9
mrnobody2002 <filipemelo032@gmail.com>
2020-03-19 12:06:07 -0300

    API setups

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const Routes = require("./src/routes");
 
-const Routes = require('./src/routes')
+mongoose.connect(
+  "mongodb+srv://root:root@cluster0-ipxq5.mongodb.net/pc?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
-mongoose.connect('mongodb+srv://root:root@cluster0-ipxq5.mongodb.net/pc?retryWrites=true&w=majority', {
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
-app.use(cors())
-app.use(express.json())
-app.use(Routes)
-app.listen(3000, ()=>console.log('your server is running at 3000'))
+app.use("/files", express.static(path.resolve(__dirname, "uploads")));
+app.use(cors());
+app.use(express.json());
+app.use(Routes);
+app.listen(3100, () => console.log("your server is running at 3100"));
