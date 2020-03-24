239438c216f73ef8fdb90c3f1c94507ec0fce0f8
mrnobody2002 <filipemelo032@gmail.com>
2020-03-24 16:49:23 -0300

    Sessions Controller handler

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,20 +1,25 @@
-const Admin = require('../models/Admin')
-
+const connection = require('../database/connection')
+const bcrypt = require('bcrypt')
 module.exports = {
-    async index(req, res){
-        const {user, pass} = req.body
+  async create(req, res) {
+    const { user, pass } = req.body
+    if (user && pass) {
+      const admin = await connection('admins')
+        .where('user', user)
+        .select('*')
+        .first()
 
-        if(user && pass){
-            const admin = await  Admin.findOne({
-                user,
-                pass
-            }) 
-            
-            if(admin){
-                res.status(200).json(admin)
-            }
-        }else{
-            return res.status(400).json({message:"Not found user"})
-        }    
-    },
+      if (!admin) res.status(400).json({ message: "User not found" })
+      const matching_passwords = await bcrypt.compare(pass, admin.password_hash)
+
+      if (!matching_passwords) res.status(400).json({ message: "Verify your password" })
+
+      const { admin_authorization } = admin
+      res.status(200).json({ admin_authorization })
+
+
+    } else {
+      return res.status(400).json({ message: "Missing parameters" })
+    }
+  },
 }
