85f2a7fbf9c5c22429a2640e54d2ec179f10feff
mrnobody2002 <filipemelo032@gmail.com>
2020-03-28 15:04:29 -0300

    category router

--- src/routes.js
+++ src/routes.js
@@ -13,6 +13,6 @@ const AdminController = require('./controllers/AdminController')
 routes.post("/admins", AdminController.create);
 routes.post('/sessions', SessionController.create)
 routes.post("/pictures", upload.single("picture"), PictureController.store);
-routes.get("/pictures", PictureController.index);
+routes.get("/pictures/:category", PictureController.index);
 routes.delete("/pictures/:id/:picture", PictureController.delete);
 module.exports = routes;
