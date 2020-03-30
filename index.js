262c6346252926133534e21735cc1c00bc35e45b
mrnobody2002 <filipemelo032@gmail.com>
2020-03-30 17:52:44 -0300

    CORS allowing befores

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const path = require("path");
 const cors = require("cors");
 const Routes = require("./src/routes");
 
-
-app.use("/files", express.static(path.resolve(__dirname, "uploads")));
 app.use(cors());
+app.use("/files", express.static(path.resolve(__dirname, "uploads")));
 app.use(express.json());
 app.use(Routes);
 app.listen(process.env.PORT || 3100, () =>
