4e6a1f10437b87f5f5c60925639f3546409592f8
mrnobody2002 <filipemelo032@gmail.com>
2020-03-30 22:31:28 -0300

    celebrate errors handling

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const app = express();
 const path = require("path");
 const cors = require("cors");
 const Routes = require("./src/routes");
+const { errors } = require("celebrate");
 
 app.use(cors());
 app.use("/files", express.static(path.resolve(__dirname, "uploads")));
 app.use(express.json());
 app.use(Routes);
+app.use(errors());
+
 app.listen(process.env.PORT || 3100, () =>
   console.log("your server is running")
 );
