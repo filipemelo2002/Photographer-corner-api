696f640c53c0c1c9630fd8991d6724bf1760638e
mrnobody2002 <filipemelo032@gmail.com>
2020-03-30 22:32:54 -0300

    Database files

--- src/database/migrations/20200324172805_update_pictures_table.js
+++ src/database/migrations/20200324172805_update_pictures_table.js
@@ -1,9 +1,9 @@
-exports.up = function (knex) {
-  return knex.schema.alterTable('pictures', (table) => {
-    table.string('picture_id')
-  })
+exports.up = function(knex) {
+  return knex.schema.alterTable("pictures", table => {
+    table.string("picture_id");
+  });
 };
 
-exports.down = function (knex) {
-  return knex.schema.dropTable('pictures')
+exports.down = function(knex) {
+  return knex.schema.dropTable("pictures");
 };
