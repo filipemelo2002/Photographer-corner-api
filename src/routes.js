2be681c40b16845cca49188889e810c48017237a
mrnobody2002 <filipemelo032@gmail.com>
2020-03-30 22:31:58 -0300

    Celebrate Validating data

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-
+const { celebrate, Joi, Segments } = require("celebrate");
 const routes = Router();
 
 const uploadConfig = require("./config/upload");
@@ -8,11 +8,37 @@ const upload = multer(uploadConfig);
 
 const SessionController = require("./controllers/SessionController");
 const PictureController = require("./controllers/PictureController");
-const AdminController = require('./controllers/AdminController')
+const AdminController = require("./controllers/AdminController");
 
-routes.post("/admins", AdminController.create);
-routes.post('/sessions', SessionController.create)
+routes.post(
+  "/admins",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      user: Joi.string().required(),
+      pass: Joi.string().required()
+    })
+  }),
+  AdminController.create
+);
+routes.post(
+  "/sessions",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      user: Joi.string().required(),
+      pass: Joi.string().required()
+    })
+  }),
+  SessionController.create
+);
 routes.post("/pictures", upload.single("picture"), PictureController.store);
-routes.get("/pictures/:category", PictureController.index);
+routes.get(
+  "/pictures/:category",
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number()
+    })
+  }),
+  PictureController.index
+);
 routes.delete("/pictures/:id/:picture", PictureController.delete);
 module.exports = routes;
